4ec924b67c937bd8c54f4fdfb9ef08d1fcee17ef
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-03-21 14:37:49 +0700

    refactor: use edge runtime and detach slash-create

--- api/interactions.ts
+++ api/interactions.ts
@@ -1,27 +1,56 @@
-// This is ran upon interaction
+import nacl from 'tweetnacl'
+import { Buffer } from 'buffer'
 
-import { SlashCreator, VercelServer } from 'slash-create'
-import path from 'path'
+export const config = {
+  runtime: 'edge',
+  regions: ['ap-southeast-1']
+}
 
-export const creator = new SlashCreator({
-  applicationID: process.env.DISCORD_APP_ID as string,
-  publicKey: process.env.DISCORD_PUBLIC_KEY,
-  token: process.env.DISCORD_BOT_TOKEN
-})
+export const getApplicationEnvironment = () =>
+  process.env.NODE_ENV ?? 'production'
+export const isDevelopment = () => getApplicationEnvironment() === 'development'
 
-creator
-  .withServer(new VercelServer())
-  .registerCommandsIn(path.join(__dirname, '..', 'commands'), ['.ts'])
+export default async (req: Request) => {
+  if (req.method !== 'POST')
+    return new Response(JSON.stringify({ message: 'method not allowed!' }), {
+      status: 405
+    })
 
-creator.on('warn', (message) => console.warn(message))
-creator.on('error', (error) => console.error(error))
-creator.on('commandRun', (command, _, ctx) =>
-  console.info(
-    `${ctx.user.username}#${ctx.user.discriminator} (${ctx.user.id}) ran command ${command.commandName}`
+  const PUBLIC_KEY = process.env.DISCORD_PUBLIC_KEY
+  if (!PUBLIC_KEY)
+    return new Response(
+      JSON.stringify({
+        message: isDevelopment()
+          ? 'discord interaction public key is not set!'
+          : 'internal server error'
+      }),
+      {
+        status: 500
+      }
+    )
+
+  const signature = req.headers.get('X-Signature-Ed25519')
+  const timestamp = req.headers.get('X-Signature-Timestamp')
+  const body = await req.json()
+
+  if (!signature || !timestamp || !body)
+    return new Response(
+      JSON.stringify({ message: 'missing request signature' }),
+      { status: 401 }
+    )
+
+  const isVerified = nacl.sign.detached.verify(
+    Buffer.from(timestamp + JSON.stringify(body)),
+    Buffer.from(signature, 'hex'),
+    Buffer.from(PUBLIC_KEY, 'hex')
   )
-)
-creator.on('commandError', (command, error) =>
-  console.error(`Command ${command.commandName}:`, error)
-)
 
-export default (creator.server! as VercelServer).vercelEndpoint
+  if (!isVerified)
+    return new Response(
+      JSON.stringify({ message: 'invalid request signature' }),
+      { status: 401 }
+    )
+
+  if (body.type === 1)
+    return new Response(JSON.stringify({ type: 1 }), { status: 200 })
+}
